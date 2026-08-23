01a60bdc4313ad51820f4f7ee6588c2ac2103b9a
Bhaskar.DR <housetoplayhave@gmail.com>
2026-08-23 16:37:31 +0545

    call api

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-    const input = document.getElementById("input")
-    input.addEventListener("keydown" , (event)=>{
-        if(event.key ==="Enter"){
-            search()
-        }
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const input = document.getElementById("input");
+
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    search();
+  }
+});
+
+function search() {
+  const name = input.value;
+
+  console.log(name);
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}&units=metric`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      console.log("Temperature:", data.main.temp);
+      console.log("Weather:", data.weather[0].description);
     })
-
-function search(){
-
-    const name =input.value
-    console.log(name)
+    .catch((error) => {
+      console.log(error);
+    });
 }
